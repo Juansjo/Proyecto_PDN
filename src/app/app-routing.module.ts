37cd2d17ab1408d52258a6a82373a9a1e0cf2f38
Geily Natalia <gndurant@ufpso.edu.co>
2025-03-27 19:06:39 -0500

    Front

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,37 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes } from "@angular/router";
+import { HomeComponent } from "./home/home.component";
+import { LoginComponent } from "./login/login.component";
+import { RegisterComponent } from "./register/register.component";
+import { NgModule } from "@angular/core";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { 
+    path: '',
+    redirectTo: '/login',  // Redirige a login por defecto
+    pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
+  },
+  // Ruta comodín para manejar URLs no encontradas (opcional)
+  { 
+    path: '**', 
+    redirectTo: '/login' 
+  }
+];
+  
+  @NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+  })
 
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }
+  export class AppRoutingModule { }
