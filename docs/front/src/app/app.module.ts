4cbd7cec96e4f0057577f0b8c1bd164194ee5ae7
Geily Natalia <gndurant@ufpso.edu.co>
2025-05-21 21:43:17 -0500

    PP-19 <Inicio de Sesion con Google>

---
+++ docs/front/src/app/app.module.ts
@@ -0,0 +1,49 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
+import { FormsModule } from '@angular/forms';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './home/home.component';
+import { FirebaseInterceptor } from './interceptors/firebase.interceptor';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faGoogle, faFacebook, faGithub } from '@fortawesome/free-brands-svg-icons';
+  import { library } from '@fortawesome/fontawesome-svg-core';
+import { LibroListaComponent } from './components/libro-lista/libro-lista.component';
+import { LibroDetalleComponent } from './components/libro-detalle/libro-detalle.component';
+import { LibroFormComponent } from './components/libro-form/libro-form.component';
+import { faEye, faEyeSlash, faUserShield, faEnvelope, faLock } from '@fortawesome/free-solid-svg-icons';
+import { FirebaseModule } from './firebase.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    RegisterComponent,
+    LoginComponent,
+    HomeComponent,
+    LibroListaComponent,
+    LibroDetalleComponent,
+    LibroFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
+    HttpClientModule,
+    FontAwesomeModule,
+    FirebaseModule
+  ],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: FirebaseInterceptor, multi: true }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+  constructor() {
+    library.add(faEye, faEyeSlash, faUserShield, faEnvelope, faLock, faGoogle, faFacebook, faGithub);
+  }
+ }
---
+++ docs/front/src/app/components/login/login.component.ts
@@ -0,0 +1,114 @@
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Auth, signInWithEmailAndPassword, signInWithPopup, GoogleAuthProvider, FacebookAuthProvider, GithubAuthProvider } from '@angular/fire/auth';
+import { signal, computed } from '@angular/core';
+import { ViewChild, ElementRef } from '@angular/core';
+
+
+@Component({
+  selector: 'app-login',
+  standalone: false,
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss'],
+})
+export class LoginComponent {
+  mostrarCorreo() {
+    alert("Correo actual: " + this.email());
+  }
+
+  email = signal<string>('');
+  password= signal<string>('');
+  showPassword: boolean = false;
+
+ @ViewChild('passwordField') passwordField!: ElementRef<HTMLInputElement>;
+
+  togglePasswordVisibility(): void {
+    const passwordField = document.getElementById('password') as HTMLInputElement;
+    if (passwordField) { 
+      this.showPassword = !this.showPassword;
+      passwordField.type = this.showPassword ? 'text' : 'password';
+  }
+  }
+
+  constructor(private router: Router,private auth: Auth) {}
+ 
+  
+
+  async loginWithGoogle() {
+    try {
+      const provider = new GoogleAuthProvider();
+      const result = await signInWithPopup(this.auth, provider);
+      const token = await result.user.getIdToken();
+      localStorage.setItem('firebaseToken', token);
+      this.router.navigate(['/home']);
+    } catch (error) {
+      alert("Error: " + (error as Error).message);
+    }
+  }
+
+  
+
+
+ async Login() {
+  console.log("Correo capturado:", this.email);
+  try {
+    const email = this.email   
+    
+    const password = this.password
+   
+    const userCredential = await signInWithEmailAndPassword(
+      this.auth,
+      this.email(),
+      this.password()
+    );
+
+    const token = await userCredential.user.getIdToken();
+    localStorage.setItem('firebaseToken', token); // Guarda el token
+    this.router.navigateByUrl('/home');
+  } catch (error: any) {
+    if (error instanceof Error) {
+      alert("Error de autenticación: " + error.message);
+    } else {
+      alert("Error desconocido");
+    }
+    console.error("Error detallado de Firebase:", (error as any).code);
+  alert("Error: " + (error as Error).message);
+  }
+}
+ 
+ 
+ 
+  /*
+    console.log(this.email);
+    console.log(this.password);
+ 
+    let bodyData = {
+      email: this.email,
+      password: this.password,
+    };
+ 
+        this.http.post("http://localhost:8085/api/v1/employee/login", bodyData).subscribe(  (resultData: any) => {
+        console.log(resultData);
+ 
+        if (resultData.message == "Email not exits")
+        {
+      
+          alert("Email not exits");
+    
+ 
+        }
+        else if(resultData.message == "Login Success")
+    
+         {
+          this.router.navigateByUrl('/home');
+        }
+        else
+        {
+          alert("Incorrect Email and Password not match");
+        }
+
+      });
+    }
+*/
+}
